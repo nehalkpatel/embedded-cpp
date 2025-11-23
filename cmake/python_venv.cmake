cb3fdbf52f6f6dcad0dca9fc839401ca6cb83e77
Nehal Patel <hereisnehal@gmail.com>
2025-11-22 17:22:50 -0800

    Fixes venv creation

--- cmake/python_venv.cmake
+++ cmake/python_venv.cmake
@@ -2,54 +2,24 @@ function (configure_venv venv_name requirements_file)
     find_package(Python3 COMPONENTS Interpreter REQUIRED)
     set(venv_path "${CMAKE_BINARY_DIR}/${venv_name}")
 
-    add_custom_command(
-        OUTPUT ${venv_path}
-        COMMAND Python3::Interpreter -m venv ${venv_path}
-        DEPENDS
-            ${requirements_file}
-        COMMENT "Creating virtual environment ${venv_name} at ${venv_path}"
+    if(NOT EXISTS ${venv_path})
+        execute_process(
+            COMMAND ${Python3_EXECUTABLE} -m venv ${venv_path}
+            RESULT_VARIABLE venv_creation_result
+        )
+        if(NOT venv_creation_result EQUAL 0)
+            message(FATAL_ERROR "Failed to create virtual environment ${venv_name} at ${venv_path}")
+        endif()
+    endif()
+
+    execute_process(
+        COMMAND ${venv_path}/bin/pip install -q -r ${requirements_file}
+        RESULT_VARIABLE pip_install_result
     )
+    if(NOT pip_install_result EQUAL 0)
+        message(FATAL_ERROR "Failed to install packages from ${requirements_file} into virtual environment ${venv_name}")
+    endif()
 
-    add_custom_command(
-        OUTPUT ${venv_path}/.venv_activated
-        COMMAND /bin/bash -c 'source ${venv_path}/bin/activate'
-        COMMAND touch ${venv_path}/.venv_activated
-        DEPENDS
-            ${venv_path}
-        COMMENT "Activating virtual environment ${venv_name} with shell '$ENV{0}'"
-    )
+    set(${venv_name}_PYTHON ${venv_path}/bin/python3 PARENT_SCOPE)
 
-    set(ENV{VIRTUAL_ENV} ${venv_path})
-    set(ENV{PATH} ${venv_path}/bin:$ENV{PATH})
-    set(ENV{PYTHONPATH} ${venv_path}/lib/:$ENV{PYTHONPATH})
-
-    add_custom_command(
-        OUTPUT ${venv_path}/.requirements_installed
-        COMMAND ${venv_path}/bin/python3 -m pip install -r ${CMAKE_CURRENT_SOURCE_DIR}/${requirements_file}
-        COMMAND touch ${venv_path}/.requirements_installed
-        DEPENDS
-            ${requirements_file}
-            ${venv_path}
-            ${venv_path}/.venv_activated
-        COMMENT "Installing requirements for ${venv_name}"
-    )
-
-    add_custom_command(
-        OUTPUT ${venv_path}/.pip_list
-        COMMAND ${venv_path}/bin/python3 -m pip list --format=columns > ${venv_path}/.pip_list
-        DEPENDS
-            ${venv_path}/.requirements_installed
-        COMMENT "Listing installed packages for ${venv_name}"
-    )
-
-    add_custom_target(
-        ${venv_name}
-        ALL
-        DEPENDS
-            ${venv_path}/.requirements_installed
-            ${venv_path}/.pip_list
-    )
-
-    set_property(TARGET ${venv_name} PROPERTY VIRTUAL_ENVIRONMENT TRUE)
-    set_property(TARGET ${venv_name} PROPERTY VIRTUAL_ENVIRONMENT_DIRECTORY ${venv_path})
 endfunction()
